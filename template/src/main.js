b6c6921d3af36f69c3d7435763fadbf1a1e8f432
令人发指哎 <x020198@qq.com>
2018-05-23 14:38:48 +0800

    修复一些bug

--- template/src/main.js
+++ template/src/main.js
@@ -12,11 +12,7 @@ new Vue({
   {{#router}}
   router,
   {{/router}}
-  // {{#if_eq build "runtime"}}
-  // render: h => h(App)
-  // {{/if_eq}}
-  // {{#if_eq build "standalone"}}
   components: { App },
   template: '<App/>'
-  // {{/if_eq}}
 })
+
